2e45240c46584ed65a656cc4e5e14cadce498d17
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-05 14:54:08 +0000

    MONDATORY

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 2-do_deploy_web_static.py) that
-creates and distributes an archive to your web servers, using the function deploy"""
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+"""
 
 from fabric.api import env, local, put, run
 from datetime import datetime
