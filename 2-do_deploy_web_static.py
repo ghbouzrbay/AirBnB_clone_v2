3df1628b97a1f9e8471e060abde1d434b1e01b34
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-05 15:07:50 +0000

    ADVANCED

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy"""
+that distributes an archive to your web servers"""
 
 from fabric.api import put, run, env
 from os.path import exists
