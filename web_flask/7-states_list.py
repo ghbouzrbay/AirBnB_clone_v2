9b1600560a43c304677c44c16bdac25be33b0b49
bouzrbay <atighmarcelline@gmail.com>
2023-10-21 19:56:58 +0100

    ):

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,22 +4,23 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models import storage
-
 app = Flask(__name__)
-	
-@app.teardown_appcontext
-def teardown_session(exception):
-    storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
-    return render_template('states_list.html', states=sorted_states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
+
