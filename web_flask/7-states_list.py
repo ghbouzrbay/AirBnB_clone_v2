4b1cf717df050aef5836a7a03e256b68a5e4277d
bouzrbay <atighmarcelline@gmail.com>
2023-10-21 18:46:09 +0100

    Don

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """removes the current SQLAlchemy Session
-    """
-    if storage is not None:
-        storage.close()
-
-
-@app.route('/states_list')
-def states_list(n=None):
-    """displays a HTML page: inside the tag BODY"""
-    states = storage.all('State')
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """removes the current SQLAlchemy Session
-    """
-    if storage is not None:
-        storage.close()
-
-
-@app.route('/cities_by_states')
-def cities_list(n=None):
-    """displays a HTML page: inside the tag BODY"""
-    states = storage.all('State').values()
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
