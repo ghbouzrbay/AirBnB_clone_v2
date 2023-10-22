5db5b3bc0828229d0ca17a8a8c5b0074ba1fa05d
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-22 10:40:35 -0700

    Ok

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,25 +3,28 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
+@app.route('/states', strict_slashes=False)
+def states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists"""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", states=states)
+    return render_template("9-states.html")
 
 @app.teardown_appcontext
-def dispose(exception):
-    """ Remove current session """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states/')
-@app.route('/states/<id>')
-def states_and_state(id=None):
-    """ Display list of all the states """
-    if id:
-        id = 'State.{}'.format(id)
-    return render_template('9-states.html', states=storage.all(State), id=id)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
