0cce1268055a45b1503c2a92c2bd068e926f9000
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-22 10:17:35 -0700

    Ok

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,25 +4,27 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models.state import state
+from models.state import State
 from models import storage
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
+    """display a HTML page with the state objects in DBStorage
+	sorted by name (A->Z)
+    """
     states = sorted(storage.all(State).values(), key=lambda s: s.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """remove the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
 
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Starts a Flask web app """
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def dispose(exception):
+    """ Remove current session """
     storage.close()
 
+
+@app.route('/states/')
+@app.route('/states/<id>')
+def states_and_state(id=None):
+    """ Display list of all the states """
+    if id:
+        id = 'State.{}'.format(id)
+    return render_template('9-states.html', states=storage.all(State), id=id)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
