60c2b30d6ace8f67afc5c19c3c956fdaeb045a3e
bouzrbay <atighmarcelline@gmail.com>
2023-10-22 21:52:57 +0100

    push

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -25,6 +25,6 @@ def teardown(exception):
     """remove the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 
