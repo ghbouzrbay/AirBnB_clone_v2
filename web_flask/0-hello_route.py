8d4a47c9fbe6db362ac39d0f9e8276f5077a181e
ghbouzrbay <atighmarcelline@gmail.com>
2023-11-26 13:58:22 +0000

    configuration

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/airbnb-onepage/')
 def index():
     """display "Hello HBNB!"
 
@@ -18,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=None)
